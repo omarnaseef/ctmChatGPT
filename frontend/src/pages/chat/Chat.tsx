dceb98f94762776eee467f85af9475495c225a85
Omar Naseef <139890014+omarnaseef@users.noreply.github.com>
2023-09-08 09:50:02 -0500

    Update Chat.tsx
    Modified text to make it specific to CTM

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -544,8 +544,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>CTM Secure ChatGPT</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Click here for instructions on how to use this Chatbot.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
